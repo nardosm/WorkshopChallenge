503c1cf393375691b08b93500e9405d92b121dbf
Nardos Hailu <nardosmgrs@gmail.com>
2017-08-01 20:58:56 -0400

    Added code to render sample table using a remote json file

--- workshop_challenge/src/App.js
+++ workshop_challenge/src/App.js
@@ -1,65 +1,97 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import './App.css';
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import {Dimmer, Loader, Table } from 'semantic-ui-react'
+
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      column: null,
+      data: [],
+      direction: null,
+      loading: true
+    };
+  }
+
+  componentDidMount () {
+      const url = "https://randomuser.me/api/?results=20";
+
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+            data = data.results;
+            this.setState({
+              data:data,
+              loading:false
+            });
+            console.log(data);
+        })
+  }
+
+
+  handleSort = clickedColumn => () => {
+    const { column, data, direction } = this.state
+
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: _.sortBy(data, [clickedColumn]),
+        direction: 'ascending',
+      })
+
+      return
+    }
+
+    this.setState({
+      data: data.reverse(),
+      direction: direction === 'ascending' ? 'descending' : 'ascending',
+    })
+  }
+
   render() {
+    const { column, data, direction } = this.state
+
     return (
-      
-      <div className="page-padding">
-        <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Header</Table.HeaderCell>
-          <Table.HeaderCell>Header</Table.HeaderCell>
-          <Table.HeaderCell>Header</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>
-            <Label ribbon>First</Label>
-          </Table.Cell>
-          <Table.Cell>Cell</Table.Cell>
-          <Table.Cell>Cell</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Cell</Table.Cell>
-          <Table.Cell>Cell</Table.Cell>
-          <Table.Cell>Cell</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Cell</Table.Cell>
-          <Table.Cell>Cell</Table.Cell>
-          <Table.Cell>Cell</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-
-      <Table.Footer>
-      <Table.Row>
-        <Table.HeaderCell colSpan='3'>
-          <Menu floated='right' pagination>
-            <Menu.Item as='a' icon>
-              <Icon name='left chevron' />
-            </Menu.Item>
-            <Menu.Item as='a'>1</Menu.Item>
-            <Menu.Item as='a'>2</Menu.Item>
-            <Menu.Item as='a'>3</Menu.Item>
-            <Menu.Item as='a'>4</Menu.Item>
-            <Menu.Item as='a' icon>
-              <Icon name='right chevron' />
-            </Menu.Item>
-          </Menu>
-        </Table.HeaderCell>
-      </Table.Row>
-      </Table.Footer>
-    </Table>
-
-</div>
 
 
-    );
+      <div className= "page-padding">
+
+          <Dimmer active = {this.state.loading} inverted>
+            <Loader>Loading</Loader>
+          </Dimmer>
+
+
+          <Table sortable celled fixed>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell sorted={column === 'phone' ? direction : null} onClick={this.handleSort('phone')}>
+                  Phone
+                </Table.HeaderCell>
+                <Table.HeaderCell sorted={column === 'email' ? direction : null} onClick={this.handleSort('email')}>
+                  Email
+                </Table.HeaderCell>
+
+                <Table.HeaderCell sorted={column === 'cell' ? direction : null} onClick={this.handleSort('cell')}>
+                  Cell
+                </Table.HeaderCell>
+
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {_.map(data, ({ phone, email, cell }) => (
+                <Table.Row key={phone}>
+                  <Table.Cell>{phone}</Table.Cell>
+                  <Table.Cell>{email}</Table.Cell>
+                   <Table.Cell>{cell}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+    )
   }
 }
 
